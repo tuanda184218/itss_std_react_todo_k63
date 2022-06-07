7ed988f95a812d241af307fd23b33675a88e4db5
tuan184218 <tuanpeterkill@gmail.com>
2022-06-08 03:01:34 +0700

    create by other components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 /* 
   【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
+ ・key：Todoを特定するID（String）
+ ・text：Todoの内容（String）
+ ・done：完了状態（Boolean true:完了済み,, false:未完了）
 */
 
 /* コンポーネント */
@@ -16,11 +16,11 @@ import Filter from './Filter';
 import useStorage from '../hooks/storage';
 
 /* ライブラリ */
-import {getKey} from "../lib/util";
+import { getKey } from "../lib/util";
 
 function Todo() {
   const [items, putItems] = React.useState([
-      /* テストコード 開始 */
+    /* テストコード 開始 */
     { key: getKey(), text: '日本語の宿題', done: false },
     { key: getKey(), text: 'reactを勉強する', done: false },
     { key: getKey(), text: '明日の準備をする', done: false },
@@ -33,10 +33,9 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+        <TodoItem key={item.key}
+          item={item}
+        />
       ))}
       <div className="panel-block">
         {items.length} items
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,14 @@
 /* 
   【TodoItemコンポーネント】
-　・Todoアイテムを表示する
-　・チェックボックスにチェックが入っているか管理する
-　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
+ ・Todoアイテムを表示する
+ ・チェックボックスにチェックが入っているか管理する
+ ・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem(  ) {
+function TodoItem({ item }) {
   return (
-    <label className="panel-block">
+    <label className="panel-block ">
+      <input type="checkbox" />
+      {item.text}
     </label>
   );
 }
